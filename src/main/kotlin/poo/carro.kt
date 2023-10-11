ee0c7e34f91a6b97a6178fd67ebcbe3c5952e0c0
Vitor <vitorloula3@gmail.com>
2023-10-11 15:32:33 -0300

    Poo com Kotlin

---
+++ src/main/kotlin/poo/carro.kt
@@ -0,0 +1,56 @@
+package poo
+
+class Carro {
+    private var pass: Int = 0
+    private var passMax: Int = 2
+    private var gas: Int = 0
+    private var gasMax: Int = 100
+    private var km: Int = 0
+
+    init {
+        pass = 0
+        gas = 0
+        km = 0
+    }
+
+    fun enter(){
+        if(pass < passMax){
+            pass++
+        }else {
+            println("Limite de passageiros preenchido: $pass")
+        }
+    }
+
+    fun leave(){
+        if(pass > 0){
+            pass--
+        }else{
+            println("Nao tem passageiros pra sair: $pass")
+        }
+    }
+
+    fun drive(qtdKm:Int){
+        if(qtdKm <= gas && pass > 0){
+            gas -= qtdKm
+            km = qtdKm
+        }else if (pass == 0){
+            println("Nao tem ninguem no carro")
+        }else if (qtdKm> gas){
+            println("Nao tem combustivel suficiente")
+        }
+    }
+
+    fun fuel(qtdLt:Int){
+        if(gas + qtdLt > gasMax){
+            gas = gasMax
+        }else{
+            gas += qtdLt
+        }
+    }
+
+    override fun toString(): String {
+        return ("pass: $pass, gas: $gas, km: $km")
+    }
+
+}
+
---
+++ src/main/kotlin/poo/mainCarro.kt
@@ -0,0 +1,53 @@
+package poo
+
+fun main() {
+    val car = Carro()
+
+    while (true) {
+        println(""" 
+                    /// Menu ///
+            
+            -> show = mostra estado do carro
+            -> leave = retira uma pessoa do carro
+            -> enter = insere uma pessoa no carro
+            -> fuel = abastecer carro
+            -> drive = dirigir carro
+        """.trimMargin())
+        val opcao = readln()
+
+        when (opcao) {
+
+            "show" -> {
+                println(car.toString())
+            }
+
+            "enter" -> {
+                car.enter()
+            }
+
+            "leave" -> {
+                car.leave()
+            }
+
+            "drive" -> {
+                println("Quantos Km o carro vai andar?")
+                val value: Int = readln().toIntOrNull() ?: -1
+                car.drive(value)
+            }
+
+            "fuel" -> {
+                println("Quantos litros vai abastecer o carro?")
+                val value: Int = readln().toIntOrNull() ?: -1
+                car.fuel(value)
+            }
+
+            "end" -> {
+                return
+            }
+
+            else -> {
+                println("fail: comando invalido")
+            }
+        }
+    }
+}
